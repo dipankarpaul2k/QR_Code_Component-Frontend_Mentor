bc156a1f2029335596454d515f673501697a1393
dipankarpaul2k <dipankarpaul2k@gmail.com>
2024-04-29 13:10:45 +0530

    added custom font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
